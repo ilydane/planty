56a3da6020d44d141235c832e480a73a63e59e87
ilydane <leoo75018@gmail.com>
2024-11-18 21:03:17 +0100

    dernier ajustement page commande

--- wp-content/plugins/elementor/vendor/composer/installed.php
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'elementor/elementor',
         'pretty_version' => '3.25.x-dev',
         'version' => '3.25.9999999.9999999-dev',
-        'reference' => 'db742591d9c44febb73303b7d7d2a3e5227d655d',
+        'reference' => '5c7d85bfded98ea15c6c1ae77b035f93619cb765',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'elementor/elementor' => array(
             'pretty_version' => '3.25.x-dev',
             'version' => '3.25.9999999.9999999-dev',
-            'reference' => 'db742591d9c44febb73303b7d7d2a3e5227d655d',
+            'reference' => '5c7d85bfded98ea15c6c1ae77b035f93619cb765',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
